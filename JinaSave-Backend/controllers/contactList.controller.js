8780d5bbd8a15d7784f923f77b443f74b05e682d
Gichuki Muchiri <collinsgichuki01@gmail.com>
2024-10-29 13:31:11 +0300

    Add routes and controllers for ContactList

--- JinaSave-Backend/controllers/contactList.controller.js
+++ JinaSave-Backend/controllers/contactList.controller.js
@@ -25,4 +25,44 @@ const getAllContactLists = async (req, res) => {
     }
 };
 
-module.exports = { getAllContactLists };
+const addContactList = async (req, res) => {
+    const newList = new ContactList({
+        name: req.body.name,
+    });
+
+    try {
+        const savedList = await newList.save();
+        res.status(200).json(savedList);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+const deleteContactList = async (req, res) => {
+    try {
+        const contactList = await ContactList.findByIdAndDelete(req.params.id);
+
+        if (!contactList) return res.status(404).json({ message: 'Contact list not found' });
+
+        res.status(200).json({message: 'Contact deleted successfully'});
+    } catch (err) {
+        if (err.kind === 'ObjectId') return res.status(404).json({message: 'Contact not found'});
+
+        res.status(500).json({message: err.message});
+    }
+};
+
+const updateContactList = (req, res) => {
+    ContactList.findByIdAndUpdate(req.params.id, req.body, { new: true })
+       .then(updatedList => {
+            if (!updatedList) return res.status(404).json({ message: 'Contact list not found' });
+            res.status(200).json(updatedList);
+        })
+       .catch(err => {
+            if (err.kind === 'ObjectId') return res.status(404).json({ message: 'Contact list not found' });
+            res.status(500).json({ message: err.message });
+        });
+};
+
+
+module.exports = { getAllContactLists, addContactList, deleteContactList, updateContactList };
--- JinaSave-Backend/routes/contactList.routes.js
+++ JinaSave-Backend/routes/contactList.routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const { getAllContactLists } = require('../controllers/contactList.controller.js');
+const { getAllContactLists, addContactList, deleteContactList, updateContactList } = require('../controllers/contactList.controller.js');
 const router = express.Router();
 
 router.get('/', getAllContactLists);
+router.post('/', addContactList);
+router.delete('/:id', deleteContactList);
+router.put('/:id', updateContactList);
 
 module.exports = router;
